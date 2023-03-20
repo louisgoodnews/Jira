9001ee48227024646fa2acf12788979dde9536d8
Louis Goodnews <louisgoodnews95@gmail.com>
2023-03-20 10:04:42 +0100

    Initially created this method.

---
+++ on-premises/software/methods/getApplicationUsersFromDirectory.groovy
@@ -0,0 +1,25 @@
+import com.atlassian.jira.user.util.UserManager;
+import com.atlassian.crowd.embedded.api.Directory;
+import com.atlassian.jira.bc.JiraServiceContextImpl;
+import com.atlassian.jira.bc.JiraServiceContext;
+import com.atlassian.jira.bc.user.search.UserSearchService;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.component.ComponentAccessor;
+
+private LinkedHashMap<String, Serializable> getApplicationUsersFromDirectory(String directoryName){
+    
+    UserManager userManager = ComponentAccessor.getUserManager();
+    UserSearchService userSearchService = ComponentAccessor.getUserSearchService();
+    JiraServiceContext serviceContext = new JiraServiceContextImpl(ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser());
+    LinkedList<ApplicationUser> applicationUserList = userSearchService.findUsers(serviceContext, "") as LinkedList<ApplicationUser>;
+    List<Long> directoyIdList = applicationUserList*.getDirectoryId().unique();
+    Long relevantDirectoryId = applicationUserList*.getDirectoryId().unique().find{ Long direcotyId ->
+
+        userManager.getDirectory(direcotyId).getName().equalsIgnoreCase(directoryName);
+    };
+    LinkedList<ApplicationUser> result = applicationUserList.findAll{    ApplicationUser applicationUser ->
+
+        applicationUser.getDirectoryId() == relevantDirectoryId
+    } as LinkedList<ApplicationUser>;
+    return ["no of results" : result.size(), "results" : result];
+}
