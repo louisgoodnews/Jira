27befe591c8171aac037f3c1b3b9717a3470e41f
Louis Goodnews <louisgoodnews95@gmail.com>
2023-04-01 11:35:12 +0200

    Added an alternative method

--- on-premises/software/methods/findApplicationUserFromDirectory.groovy
+++ on-premises/software/methods/findApplicationUserFromDirectory.groovy
@@ -13,13 +13,27 @@ private LinkedHashMap<String, Serializable> findApplicationUserFromDirectory(Str
     JiraServiceContext serviceContext = new JiraServiceContextImpl(ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser());
     LinkedList<ApplicationUser> applicationUserList = userSearchService.findUsers(serviceContext, queryString) as LinkedList<ApplicationUser>;
     List<Long> directoyIdList = applicationUserList*.getDirectoryId().unique();
-    Long relevantDirectoryId = applicationUserList*.getDirectoryId().unique().find{ Long direcotyId ->
+    Long relevantDirectoryId = applicationUserList*.getDirectoryId().unique().find{ Long directoryId ->
 
-        userManager.getDirectory(direcotyId).getName().toLowerCase().contains(directoryName) || userManager.getDirectory(direcotyId).getName().toUpperCase().contains(directoryName);
+        userManager.getDirectory(directoryId).getName().toLowerCase().contains(directoryName) || userManager.getDirectory(directoryId).getName().toUpperCase().contains(directoryName);
     };
     LinkedList<ApplicationUser> result = applicationUserList.findAll{    ApplicationUser applicationUser ->
 
         applicationUser.getDirectoryId() == relevantDirectoryId
+    } as LinkedList<ApplicationUser>;
+    return ["no of results" : result.size(), "results" : result];
+}
+
+private LinkedHashMap<String, Serializable> findApplicationUserFromDirectory(String queryString, Integer directoryId){
+    
+    UserManager userManager = ComponentAccessor.getUserManager();
+    UserSearchService userSearchService = ComponentAccessor.getUserSearchService();
+    JiraServiceContext serviceContext = new JiraServiceContextImpl(ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser());
+    LinkedList<ApplicationUser> applicationUserList = userSearchService.findUsers(serviceContext, queryString) as LinkedList<ApplicationUser>;
+    List<Long> directoyIdList = applicationUserList*.getDirectoryId().unique();
+    LinkedList<ApplicationUser> result = applicationUserList.findAll{    ApplicationUser applicationUser ->
+
+        applicationUser.getDirectoryId() == directoryId
     } as LinkedList<ApplicationUser>;
     return ["no of results" : result.size(), "results" : result];
 }
